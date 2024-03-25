c35ff0a9b183e832c4f4b3a32eb3333a7a395287
git-chad <tobimocc@gmail.com>
2024-03-25 13:54:16 -0300

    chore(dev): here's what we do rework

--- app/components/home/our-work-module/firstSec.jsx
+++ app/components/home/our-work-module/firstSec.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import Container from "../../ecyce/container";
 import Image from "next/image";
 import underline from "../../../../public/underline.svg";
@@ -11,67 +11,111 @@ const FirstSec = () => {
       <Container className={"w-full py-16 grid grid-cols-12 gap-5 px-20"}>
         <div className="relative col-span-6">
           <h1 className="bold-neue text-heading">Here&apos;s what we do</h1>
-          <h2 className="text-2xl my-4">Your automation department is here to help!</h2>
-          <Image src={underline} className="absolute right-24 bottom-2"/>
-          <Image src={arrow} className="absolute -right-24 top-12 rotate-[230deg]"/>
-          <Image/>
+          <h2 className="text-2xl my-4">
+            Your automation department is here to help!
+          </h2>
+          <Image src={underline} className="absolute right-24 bottom-2" />
+          <Image
+            src={arrow}
+            className="absolute -right-24 top-12 rotate-[230deg]"
+          />
+          <Image />
         </div>
 
-        <div className="col-span-12 flex flex-col gap-10 py-16 justify-center items-center">
-            <div className="w-full flex gap-5">
-              <div className="flip-card w-full">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front w-[427px] h-[464px] border text-g-red border-g-red rounded-3.5xl flex justify-center items-center text-xl text-center">
-                    Google Workspace Automation
-                  </div>
-                  <div className="flip-card-back w-[427px] h-[464px] border bg-g-red text-g-red border-g-red rounded-3.5xl flex justify-center items-center p-2">
-                    <p className='text-white '>We create solutions in your Workspace leveraging Apps Script, Google&apos;s programming language made to interact with its&apos; tools.</p>
-                  </div>
+        <div className="col-span-12 flex flex-col gap-10 sm:py-16 py-8 justify-center items-center">
+          <div className="w-full grid grid-cols-3 gap-5">
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px]  border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  Google Workspace Automation
                 </div>
-              </div>
-
-              <div className="flip-card w-full">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front w-[427px] h-[464px] border text-g-green border-g-green rounded-3.5xl flex justify-center items-center text-xl text-center">
-                    Google Add-Ons
-                  </div>
-                  <div className="flip-card-back w-[427px] h-[464px] bg-g-green border text-g-green border-g-green rounded-3.5xl flex justify-center items-center p-2">
-                    <p className='text-white font-normal'>Deploy your SaaS product in the Google Marketplace to broaden your market and also treat your users with new ways of using your software.</p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flip-card w-full">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front w-[427px] h-[464px] border text-g-blue border-g-blue rounded-3.5xl flex justify-center items-center text-xl text-center">
-                    Data Analytics tools
-                  </div>
-                  <div className="flip-card-back w-[427px] h-[464px] bg-g-blue border text-g-blue border-g-blue rounded-3.5xl flex justify-center items-center p-2">
-                    <p className='text-white font-normal'>We&apos;ll Take care of all your data&apos;s lifecycle, from retrieving to visualization for decision making.</p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flip-card w-full">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front w-[427px] h-[464px] border text-g-yellow border-g-yellow rounded-3.5xl flex justify-center items-center text-xl text-center">
-                    Education tools
-                  </div>
-                  <div className="flip-card-back w-[427px] h-[464px] bg-g-yellow border text-g-yellow border-g-yellow rounded-3.5xl flex justify-center items-center p-2">
-                    <p className='text-sf-black font-normal'>Unlock the power of data to improve student outcomes.</p>
-                  </div>
+                <div className="flip-card-back w-[427px] bg-g-blue text-sf-cream rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    We create solutions in your Workspace leveraging Apps
+                    Script, Google&apos;s programming language made to interact
+                    with its&apos; tools.
+                  </p>
                 </div>
               </div>
             </div>
-            <Button type={'outline'} text={'See more!'}/>
+
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px]  border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  Web Apps
+                </div>
+                <div className="flip-card-back w-[427px] bg-g-red text-sf-cream rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    Create custom WebApps as internal solutions using Google
+                    Apps Script.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px] border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  Data
+                </div>
+                <div className="flip-card-back w-[427px] bg-g-yellow text-sf-black rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    Implement data warehouses in BigQuery and dashboards in
+                    Looker Studio for insightful analytics.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px]  border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  No code / App Sheet
+                </div>
+                <div className="flip-card-back w-[427px] bg-g-blue text-sf-cream rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    Develop customized applications on AppSheet to streamline
+                    business processes.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px]  border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  Google Add-Ons
+                </div>
+                <div className="flip-card-back w-[427px] bg-g-green text-sf-cream rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    Build and deploy Add-ons for Google Marketplace to extend
+                    the functionality of Google Workspace applications.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card w-full">
+              <div className="flip-card-inner">
+                <div className="flip-card-front w-[427px]  border text-sf-cream border-sf-cream rounded-3.5xl flex justify-center items-center text-xl text-center">
+                  Google Chrome Extensions
+                </div>
+                <div className="flip-card-back w-[427px] bg-g-red bg-g- text-sf-cream rounded-3.5xl flex justify-center items-center p-2">
+                  <p>
+                    Develop custom Chrome Extensions for improve productivity
+                    and user experience within the browser.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Button type={"outline"} text={"See more!"} />
         </div>
 
-        <div className="c col-span-5">
-
-        </div>
+        <div className="c col-span-5"></div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default FirstSec
+export default FirstSec;
